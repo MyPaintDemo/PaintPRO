2d41e1e01d91edcceae1b7bd5fed1369d21de5ca
daniyarka <daniyar_-k-98@mail.ru>
2019-01-12 03:35:36 +0600

    second

---
+++ PaintPRO/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace PaintPRO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(615, 233);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 48);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 48);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(255, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 22);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(174, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 48);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(336, 48);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(336, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Save";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(390, 25);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Open File";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(471, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 32);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Rectangle";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(471, 40);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 32);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Ellipse";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(552, 2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 32);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Triangle";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(552, 38);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 32);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Pipetka";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(390, 49);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "Fill";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(255, 40);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(59, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "Spray";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 316);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ PaintPRO/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PaintPRO
+{
+    public partial class Form1 : Form
+    {
+        public enum Shape
+        {
+            Free, Line, Ellipse, Rectangle, Triangle, Pipetka, Fill, Spray
+        };
+
+        Pen pen = new Pen(Color.Black, 2);
+        Graphics g;
+        GraphicsPath gp = new GraphicsPath();
+        Point current, end, start;
+        Bitmap bmp;
+        Color originColor;
+        Queue<Point> q = new Queue<Point>();
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = e.Location;
+            if (currentShape == Shape.Pipetka)
+            {
+                pen.Color = bmp.GetPixel(e.Location.X, e.Location.Y);
+                button3.BackColor = pen.Color;
+            }
+            if (currentShape == Shape.Fill)
+            {
+                originColor = bmp.GetPixel(e.Location.X, e.Location.Y);
+                FillRegion(e.Location);
+            }
+        }
+        private void Step(Point pp)
+        {
+            if(pp.X < 0 || pp.Y<0 ||pp.X >= pictureBox1.Width||pp.Y >= pictureBox1.Height)
+            {
+                return;
+            }
+            if (bmp.GetPixel(pp.X, pp.Y)!=originColor)
+            {
+                return;
+            }else
+            {
+                bmp.SetPixel(pp.X, pp.Y, pen.Color);
+                q.Enqueue(pp);
+            }
+        }
+        private void FillRegion(Point pp)
+        {
+            q.Enqueue(pp);
+            while (q.Count > 0)
+            {
+                Point cur = q.Dequeue();               
+                Step(new Point(cur.X - 1, cur.Y));
+                Step(new Point(cur.X + 1, cur.Y));
+                Step(new Point(cur.X, cur.Y + 1));
+                Step(new Point(cur.X, cur.Y - 1));
+            }
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                switch (currentShape)
+                {
+                    case Shape.Free:
+                        current = e.Location;
+                        g.DrawLine(pen, start, current);
+                        start = current;
+                        break;
+                    case Shape.Line:
+                        current = e.Location;
+                        gp.Reset();
+                        gp.AddLine(start, current);
+                        break;
+                    case Shape.Rectangle:
+                        current = e.Location;
+                        if (current.X > start.X && current.Y > start.Y) {
+                            Rectangle rec = new Rectangle(start.X, start.Y, current.X - start.X, current.Y - start.Y);
+                            gp.Reset();
+                            gp.AddRectangle(rec);
+                        }
+                        else if(current.X < start.X && current.Y < start.Y)
+                        {
+                            Rectangle rec = new Rectangle(current.X, current.Y, start.X-current.X  ,start.Y - current.Y );
+                            gp.Reset();
+                            gp.AddRectangle(rec);
+                        }else if (current.X > start.X && current.Y < start.Y)
+                        {
+                            Rectangle rec = new Rectangle(start.X, current.Y, Math.Abs(current.X-start.X), Math.Abs(current.Y-start.Y));
+                            gp.Reset();
+                            gp.AddRectangle(rec);
+                        }
+                        else if (current.X < start.X && current.Y > start.Y)
+                        {
+                            Rectangle rec = new Rectangle(current.X, start.Y, Math.Abs(start.X-current.X), Math.Abs(start.Y- current.Y));
+                            gp.Reset();
+                            gp.AddRectangle(rec);
+                        }
+                        break;
+                    case Shape.Ellipse:
+                        current = e.Location;
+                        if (current.X > start.X && current.Y > start.Y)
+                        {
+                            Rectangle rec = new Rectangle(start.X, start.Y, current.X - start.X, current.Y - start.Y);
+                            gp.Reset();
+                            gp.AddEllipse(rec);
+                        }
+                        else if (current.X < start.X && current.Y < start.Y)
+                        {
+                            Rectangle rec = new Rectangle(current.X, current.Y, start.X - current.X, start.Y - current.Y);
+                            gp.Reset();
+                            gp.AddEllipse(rec);
+                        }
+                        else if (current.X > start.X && current.Y < start.Y)
+                        {
+                            Rectangle rec = new Rectangle(start.X, current.Y, Math.Abs(current.X - start.X), Math.Abs(current.Y - start.Y));
+                            gp.Reset();
+                            gp.AddEllipse(rec);
+                        }
+                        else if (current.X < start.X && current.Y > start.Y)
+                        {
+                            Rectangle rec = new Rectangle(current.X, start.Y, Math.Abs(start.X - current.X), Math.Abs(start.Y - current.Y));
+                            gp.Reset();
+                            gp.AddEllipse(rec);
+                        }
+                        break;
+                    case Shape.Triangle:
+                        current = e.Location;
+                        Point[] points ={
+                            new Point((current.X+start.X)/2, start.Y),
+                            new Point(start.X, current.Y),
+                            new Point(current.X, current.Y),                              
+                        };
+                        gp.Reset();
+                        gp.AddPolygon(points);
+                        break;
+                    case Shape.Spray:
+                        Pen p = new Pen(button3.BackColor);                     
+                        current = e.Location;
+                        //gp.AddEllipse(new Rectangle(current.X, current.Y, (int)numericUpDown1.Value+1, (int)numericUpDown1.Value+1));
+                        Random rnd = new Random();
+                        int radius = (int)numericUpDown1.Value+1;
+                        for (int i =0;i<=100; i++)
+                        {                            
+                            double theta = rnd.NextDouble() * (Math.PI * 2);
+                            double r = rnd.NextDouble() * radius;
+                            double x = e.X + Math.Cos(theta) * r;
+                            double y = e.Y + Math.Sin(theta) * r;                                                    
+                            g.DrawEllipse(p, new Rectangle((int)x, (int)y, 1, 1));
+                        }
+                        start = current;
+                       // gp.Reset();
+                        break;                                       
+                }
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            g.DrawPath(pen, gp);
+            //gp.Reset();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawPath(pen, gp);
+            pictureBox1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Free;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Line;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            gp.Reset();
+            pen.Width = (float)numericUpDown1.Value;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {        
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = cd.Color;
+                button3.BackColor = cd.Color;
+            }
+            gp.Reset();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Pen p = new Pen(Color.White);
+            //pictureBox1.Image = null;
+            g.FillRegion(p.Brush, new Region(new Rectangle(0,0, pictureBox1.Width, pictureBox1.Height)));
+            gp.Reset();
+            pictureBox1.Refresh();
+            //g = Graphics.FromImage(pictureBox1.Image);
+            //g = Graphics.FromImage(pictureBox1.Image);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] ar = new byte[ms.Length];
+            ms.Write(ar, 0, ar.Length);
+            */
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {                
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                //pictureBox1.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Refresh();
+                gp.Reset();
+                g = Graphics.FromImage(pictureBox1.Image);
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Rectangle;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Ellipse;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Triangle;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Pipetka;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Fill;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            currentShape = Shape.Spray;
+        }
+
+        Shape currentShape = Shape.Free;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+        }
+    }
+}
